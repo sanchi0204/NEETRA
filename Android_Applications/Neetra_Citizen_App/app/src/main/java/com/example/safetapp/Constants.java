451a32747c5b80ec9daeee240a97e5f0f8d4d736
sanchi0204 <sanchirastogi02@gmail.com>
2020-10-14 23:43:34 +0530

    Create Constants.java

---
+++ Android_Applications/Neetra_Citizen_App/app/src/main/java/com/example/safetapp/Constants.java
@@ -0,0 +1,14 @@
+package com.example.safetapp;
+
+public class Constants {
+
+    public static final int LOCATION_UPDATE_DURATION = 30000;
+    public static final String STRESS_CALL_NUMBER = "8800360986";
+
+
+    //To keep all constants at one place
+    static final String PRIMARY_CHANNEL_ID = "FEMME CHANNEL";
+    static final CharSequence CHANNEL_NAME = "FEMME SERVICE";
+    static String USERNAME = "Test user";                           //TODO:change username set data actual data after login
+    static boolean isStressSignalSent = false;
+}
